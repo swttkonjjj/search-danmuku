91aed72de07c1df1212381a2a799b20c7c994529
swtt <swttkonjjj@foxmail.com>
2022-11-12 23:12:49 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+import { Search } from '@element-plus/icons-vue'
+import get from 'axios'
+
+const input = ref('')
+let anime = ref('')
+const animes = ref()
+const api = 'https://api.dandanplay.net/api/v2/search/episodes'
+const bgm = 'https://api.dandanplay.net/api/v2/bangumi/'
+const url = "https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+function search() {
+  get(api, {
+    params: {
+      anime: input.value
+    }
+  }).then((response) => {
+    animes.value = response.data.animes
+    console.log(response)
+    for (let i in animes.value) {
+      get(bgm + animes.value[i].animeId).then(
+        (response) => {
+          animes.value[i].imageUrl = response.data.bangumi.imageUrl
+          // animes.value[i].episodes = response.data.bangumi.episodes
+          // console.log(response)
+        }
+      )
+    }
+  })
+}
+
+</script>
+
+<template>
+
+  <el-container>
+    <el-input v-model="input" placeholder="Please Input" />
+    <el-button :icon="Search" @click="search()">Search</el-button>
+  </el-container>
+<div v-for=" anime in animes">
+  <HelloWorld :name="anime.animeTitle" :img-url="anime.imageUrl" :episodes= anime.episodes />
+  <!-- {{anime.animeTitle}}{{anime.imageUrl}} -->
+</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { ref, watch, toRefs } from 'vue'
+import get from 'axios'
+
+const pops = defineProps(['imgUrl', 'name', 'episodes'])
+const dialogTableVisible = ref(false)
+const data = ref()
+function showEp(eps: any) {
+  // console.log(eps)
+  dialogTableVisible.value = true
+  data.value = pops.episodes
+  console.log(data.value)
+}
+
+</script>
+
+<template>
+  <el-card :body-style="{ padding: '0px' }">
+    <img :src="pops.imgUrl" class="image" />
+    <div style="padding: 14px">
+      <span>{{ name }}</span>
+      <div class="bottom">
+        <!-- <time class="time">{{ currentDate }}</time> -->
+        <el-button text class="button" @click='showEp'>展开</el-button>
+      </div>
+    </div>
+  </el-card>
+
+  <el-dialog v-model="dialogTableVisible">
+    <el-table :data="data">
+      <el-table-column property="episodeTitle" label="集数标题" width="400" />
+    </el-table>
+  </el-dialog>
+</template>
+
+
